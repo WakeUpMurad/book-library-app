973cd4de41c0d2cfdede91ef847e65f40379237f
GAHRAMANOV.MU.IL.OG <01704123@sberbank.ru>
2024-05-14 22:53:57 +0300

    Filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { BsBookmarkStarFill } from 'react-icons/bs'
-import { BsBookmarkStar } from 'react-icons/bs'
+import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 import { deleteBook, toogleFavoriteBook } from '../../redux/books/actionCreators'
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
+  const titleFilter = useSelector(selectTitleFilter)
   const dispatch = useDispatch()
 
   const handleDeleteBook = (id) => {
@@ -16,6 +17,11 @@ const BookList = () => {
     dispatch(toogleFavoriteBook(id))
   }
 
+  const filteredBooks = books.filter((book) => {
+    const matchesTitle = book.title.toLowerCase().includes(titleFilter.toLowerCase())
+    return matchesTitle
+  })
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -23,7 +29,7 @@ const BookList = () => {
         <p>No books availiable</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,9 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { setTitleFilter, selectTitleFilter, resetFilters } from '../../redux/slices/filterSlice'
 import './Filter.css'
 
 const Filter = () => {
+  const dispatch = useDispatch()
+  const titleFilter = useSelector(selectTitleFilter)
+
+  const handleTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value))
+  }
+
+  const handlleResetFilters = () => {
+    dispatch(resetFilters())
+  }
   return (
     <div className="app-block filter">
-      <h2>Filters</h2>
+      <div className="filter-row">
+        <div className="filter-group">
+          <input
+            type="text"
+            placeholder="Filter by title..."
+            onChange={handleTitleFilterChange}
+            value={titleFilter}
+          />
+        </div>
+        <button
+          type="button"
+          onClick={handlleResetFilters}
+        >
+          Reset Filters
+        </button>
+      </div>
     </div>
   )
 }
